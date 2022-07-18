99e81822c46ebffa14a791fc497111bdc6aa3938
Minseok Kim <comko123@naver.com>
2022-07-19 02:16:53 +0900

    Study Project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <BrowserRouter>
     <Routes>
-    <Route path="/" element={<Page/>} />
+    <Route path="/" element={<Page/>}/>
     </Routes>
     </BrowserRouter>
   );
