5dff8ed66b08a8f150f16ed4debc0441fcf163e7
Minseok Kim <comko123@naver.com>
2022-07-19 02:09:58 +0900

    Study Project

---
+++ src/MoviePage.js
@@ -0,0 +1,11 @@
+const MoviePage = ({img,genres,sum,title,rat}) => {
+return (<>
+<h1>제목 : {title}</h1>
+<h3>평점 : {rat}</h3>
+<img src = {img} alt = {rat}/>
+<ul>{genres.map(G=><li key = {G}>{G}</li>)}</ul>
+<h2>줄거리 : {sum}</h2>
+</>)
+
+}
+export default MoviePage
---
+++ src/Page.js
@@ -0,0 +1,28 @@
+import { useDeferredValue, useEffect,useState,useTransition } from "react"
+import axios from "axios"
+import MoviePage from "./MoviePage"
+const Page = () => {
+    const [data,setData] =useState([])
+    const [ispending,startTransition] = useTransition()
+    const state = useDeferredValue(data)
+useEffect(()=>{
+    startTransition(async()=>setData(await(
+    await axios(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
+    ).data.data.movies))
+},[])
+return (
+<>
+{ispending?<h1>loading</h1>:
+state.map(({medium_cover_image,genres,summary,id,title_long,rating})=>
+ <MoviePage 
+key = {id} 
+title = {title_long}
+img = {medium_cover_image}
+genres = {genres}
+sum = {summary}
+rat = {rating}/>
+)}
+</>
+)
+}
+export default Page
