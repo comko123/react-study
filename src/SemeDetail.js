a678d0fbc97efaecdef3fd46c2eef3730d28be19
Minseok Kim <comko123@naver.com>
2022-08-12 00:13:46 +0900

    added

--- src/SemeDetail.js
+++ src/SemeDetail.js
@@ -1,7 +1,7 @@
 /*eslint-disable*/
 import "./SemeDetaill.moudule.scss";
-const SemeDetail = ({like_count,description_full,title_long,runtime,rating,genres,medium_cover_image}) => {
-
+const SemeDetail = ({like_count,description_full,
+    title_long,runtime,rating,genres=[],medium_cover_image})=>{
     return(<>
     <div className="container">
     <img src={medium_cover_image}alt ="no"/>
@@ -11,7 +11,8 @@ const SemeDetail = ({like_count,description_full,title_long,runtime,rating,genre
     <li>샹영시간 :{runtime}분</li>
     <li>평점 : {rating}</li>
     <li>추천수 : {like_count}</li>
-    <li>장르: {genres && genres.map((k,index)=><span style = {{"marginLeft":"10px"}}key ={index}>{k} </span>)}</li>
+    <li>장르: { genres.map((k,index)=><span style = {{"marginLeft":"10px"}}key ={index}>{k} </span>)}</li>
+    {/*<li>장르: {genres && genres.map((k,index)=><span style = {{"marginLeft":"10px"}}key ={index}>{k} </span>)}</li>*/}
     </ul>
     <h3 className="description">상세내용 : <br/><br/>{description_full}</h3>
     </div>
